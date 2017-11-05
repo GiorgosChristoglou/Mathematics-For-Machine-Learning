fea77491cde138b31a9d00361164837fbc6aea90
George Christoglou <gc1314@sprite13.doc.ic.ac.uk>
2017-11-05 20:31:57 +0000

    Finished q1.ii

--- q1.py
+++ q1.py
@@ -7,10 +7,19 @@ def training_data():
   Y = np.cos(10*X**2) + 0.1 * np.sin(100*X)
   return N,X,Y
 
-def generate_design_polynomial(order, X, Y, N):
+def generate_design_trig(order, X, N):
+  phi = np.zeros((N, 2*order + 1))
+  phi.fill(1)
+
+  for i in range(N):
+    phi[i] = phi_trig(order, X[i])
+  
+  return phi 
+
+def generate_design_polynomial(order, X, N):
   phi = np.zeros((N, order + 1))
   phi.fill(1)
-  
+
   for i in range(N):
     for j in range(order + 1):
       phi[i][j] = X[i] ** j
@@ -20,13 +29,29 @@ def generate_design_polynomial(order, X, Y, N):
 def phi_pol(order, x):
   return np.array([x**i for i in range(order + 1)])
 
+def phi_trig(order, x):
+  phi = np.zeros(2*order + 1);
+  phi[0] = 1
+  for i in xrange(1, order + 1):
+    phi[2 * i - 1] = np.sin(2*np.pi*x*i)
+  for i in xrange(1, order + 1):
+    phi[2* i] = np.cos(2*np.pi*x*i)
+  return phi
+
 def optimal_theta_vector(phi, y):
   step1 = np.linalg.inv(np.dot(phi.transpose(), phi))
   step2 = np.dot(step1, phi.transpose())
   return np.dot(step2, y)
 
+def plot_by_order_trig(order, N, X, Y, lbl):
+  phi = generate_design_trig(order, X, N)
+  theta = optimal_theta_vector(phi, Y)
+  points_x = np.linspace(-1, 1.2, 200)
+  points_y = [np.dot(phi_trig(order, x).transpose(), theta) for x in points_x]
+  plt.plot(points_x, points_y, label=lbl)
+
 def plot_by_order_pol(order, N, X, Y, lbl):
-  phi = generate_design_polynomial(order, X, Y, N)
+  phi = generate_design_polynomial(order, X, N)
   theta = optimal_theta_vector(phi, Y)
   points_x = np.linspace(-0.3, 1.3, 200)
   points_y = [np.dot(phi_pol(order, x).transpose(), theta) for x in points_x]
@@ -40,6 +65,18 @@ def generate_plot_q1_i():
   plot_by_order_pol(2, N, X_training, Y_training, "order=2")
   plot_by_order_pol(3, N, X_training, Y_training, "order=3")
   plot_by_order_pol(11, N, X_training, Y_training, "order=11")
+
+  plt.legend(loc=3, borderaxespad=0.)
+  plt.scatter(X_training, Y_training)
+  plt.gca().set_ylim([-2,2])
+  plt.show()
+  return
+
+def generate_plot_q1_ii():
+  N, X_training, Y_training = training_data()
+  
+  plot_by_order_trig(1, N, X_training, Y_training, "order=1")
+  plot_by_order_trig(11, N, X_training, Y_training, "order=11")
 
   plt.legend(loc=3, borderaxespad=0.)
   plt.scatter(X_training, Y_training)
